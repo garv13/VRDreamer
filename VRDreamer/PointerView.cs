954d73db1b9d4df5c2ceac61d915e7baeb369dc9
akshaygupta94 <akshaygupta1994@outlook.com>
2017-03-22 21:34:57 +0530

    some changes

---
+++ VRDreamer/PointerView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace VRDreamer
+{
+    class PointerView
+    {
+
+        public int Serial { get; set; }
+        public string Title { get; set; }
+        public StorageFile File { get; set; }
+        public string Desc { get; set; }
+    }
+}
