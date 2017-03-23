25ab1026db1bc24d3181f8c64c96e23b71892329
akshaygupta94 <akshaygupta1994@outlook.com>
2017-03-23 23:45:19 +0530

    view scrape

--- VRDreamer/ViewScrape.xaml.cs
+++ VRDreamer/ViewScrape.xaml.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Devices.Geolocation;
+using Windows.Devices.Sensors;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.Media.Capture;
+using Windows.System.Display;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +30,112 @@ namespace VRDreamer
     /// </summary>
     public sealed partial class ViewScrape : Page
     {
+        MediaCapture _mediaCapture;
+        bool _isPreviewing;
+        OrientationSensor or;
+        Compass c;
+        DisplayRequest _displayRequest;
+        List<Pointer> li;
+        Geoposition pos;
         public ViewScrape()
         {
+            li = new List<Pointer>();
+            or = OrientationSensor.GetDefault();
+            c = Compass.GetDefault();
+            try
+            {
+                c.ReportInterval = 4;
+                c.ReadingChanged += C_ReadingChanged;
+                or.ReportInterval = 4;
+                or.ReadingChanged += Or_ReadingChanged;
+            }
+            catch
+            { }
+            
+            Application.Current.Suspending += Application_Suspending;
+            
             this.InitializeComponent();
         }
+
+       
+
+        private void Or_ReadingChanged(OrientationSensor sender, OrientationSensorReadingChangedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void C_ReadingChanged(Compass sender, CompassReadingChangedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await StartPreviewAsync();
+        }
+
+        protected async override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            await CleanupCameraAsync();
+        }
+        private async void Application_Suspending(object sender, SuspendingEventArgs e)
+        {
+            // Handle global application events only if this page is active
+            if (Frame.CurrentSourcePageType == typeof(MainPage))
+            {
+                var deferral = e.SuspendingOperation.GetDeferral();
+                await CleanupCameraAsync();
+                deferral.Complete();
+            }
+        }
+
+        private async Task StartPreviewAsync()
+        {
+            try
+            {
+
+                _mediaCapture = new MediaCapture();
+                await _mediaCapture.InitializeAsync();
+                PreviewControl.Source = _mediaCapture;
+
+                await _mediaCapture.StartPreviewAsync();
+                _isPreviewing = true;
+
+                _displayRequest.RequestActive();
+                DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // This will be thrown if the user denied access to the camera in privacy settings
+                System.Diagnostics.Debug.WriteLine("The app was denied access to the camera");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("MediaCapture initialization failed. {0}", ex.Message);
+            }
+        }
+        private async Task CleanupCameraAsync()
+        {
+            if (_mediaCapture != null)
+            {
+                if (_isPreviewing)
+                {
+                    await _mediaCapture.StopPreviewAsync();
+                }
+
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    PreviewControl.Source = null;
+                    if (_displayRequest != null)
+                    {
+                        _displayRequest.RequestRelease();
+                    }
+
+                    _mediaCapture.Dispose();
+                    _mediaCapture = null;
+                });
+            }
+
+        }
+
     }
 }
