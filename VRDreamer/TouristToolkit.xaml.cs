c447d3195e251154689eab618bf55eea91cf083e
akshaygupta94 <akshaygupta1994@outlook.com>
2017-04-14 21:45:21 +0530

    monum,enmt ke aslawa vision working

--- VRDreamer/TouristToolkit.xaml.cs
+++ VRDreamer/TouristToolkit.xaml.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.WindowsAzure.MobileServices;
+﻿using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.ApplicationModel;
@@ -13,6 +19,10 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Graphics.Display;
 using Windows.Media.Capture;
+using Windows.Media.Devices;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.System.Display;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
@@ -32,7 +42,8 @@ namespace VRDreamer
     /// </summary>
     public sealed partial class TouristToolkit : Page
     {
-        
+        string blobUrl;
+        bool _isFocused = false;
         private IMobileServiceTable<Pointer> Table2 = App.MobileService.GetTable<Pointer>();
         private MobileServiceCollection<Pointer, Pointer> items2;
         MediaCapture _mediaCapture;
@@ -41,6 +52,7 @@ namespace VRDreamer
         string s;
         public TouristToolkit()
         {
+            blobUrl = "";
             this.InitializeComponent();
             Application.Current.Suspending += Application_Suspending;
         }
@@ -55,22 +67,261 @@ namespace VRDreamer
 
         private async void button_Click(object sender, RoutedEventArgs e)
         {
-            var add = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
-            var httpRequestMessage = new Windows.Web.Http.HttpRequestMessage(Windows.Web.Http.HttpMethod.Get, new Uri("http://www.google.com/searchbyimage?site=search&sa=X&image_url=https://vrdreamer.blob.core.windows.net/first/pp2.jpg"));
-            httpRequestMessage.Headers.Add("User-Agent", add);
-            items2 = await Table2.ToCollectionAsync();
-            web.NavigateWithHttpRequestMessage(httpRequestMessage);
-            web.DOMContentLoaded += Web_DOMContentLoaded;       
+            using (var captureStream = new InMemoryRandomAccessStream())
+            {
+
+                await _mediaCapture.CapturePhotoToStreamAsync(ImageEncodingProperties.CreateJpeg(), captureStream);
+                using (var s = captureStream.AsStream())
+                {
+
+                    //await _mediaCapture.CapturePhotoToStorageFileAsync(ImageEncodingProperties.CreatePng(), file);
+                    var credentials = new StorageCredentials("vrdreamer", "lTD5XmjEhvfUsC/vVTLsl01+8pJOlMdF/ri7W1cNOydXwSdb8KQpDbiveVciOqdIbuDu6gJW8g44YtVjuBzFkQ==");
+                    var client = new CloudBlobClient(new Uri("https://vrdreamer.blob.core.windows.net/"), credentials);
+                    var container = client.GetContainerReference("datasetimages");
+                    //  await container.CreateIfNotExistsAsync();
+
+                    //var perm = new BlobContainerPermissions();
+                    //perm.PublicAccess = BlobContainerPublicAccessType.Blob;
+                    //await container.SetPermissionsAsync(perm);
+                    var blockBlob = container.GetBlockBlobReference(Guid.NewGuid().ToString() + ".jpeg");
+                    s.Position = 0;
+
+                    await blockBlob.UploadFromStreamAsync(s);
+                    ////await blockBlob.UploadFromFileAsync(captureStream);
+                    blobUrl = blockBlob.StorageUri.PrimaryUri.ToString();
+                    var add = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+                    var httpRequestMessage = new Windows.Web.Http.HttpRequestMessage(Windows.Web.Http.HttpMethod.Get, new Uri("http://www.google.com/searchbyimage?site=search&sa=X&image_url=" + blockBlob.StorageUri.PrimaryUri.ToString()));                
+                    httpRequestMessage.Headers.Add("User-Agent", add);
+                    //items2 = await Table2.ToCollectionAsync();
+                    web.NavigateWithHttpRequestMessage(httpRequestMessage);
+                    web.DOMContentLoaded += Web_DOMContentLoaded;
+                }
+            }
+
+             
         }
 
         private async void Web_DOMContentLoaded(WebView sender, WebViewDOMContentLoadedEventArgs args)
         {        
-           s= await web.InvokeScriptAsync("eval", new string[] { "document.documentElement.outerHTML;" });
-            int i = s.IndexOf("<a class=\"_gUb");
-            int j = s.IndexOf(">", i);
-            i = s.IndexOf("<",j);
-            s = s.Substring(j+1, i-j-1);
+            s= await web.InvokeScriptAsync("eval", new string[] { "document.documentElement.outerHTML;" });
+            try
+
+            {
+
+                int i = s.IndexOf("<a class=\"_gUb");
+                int j = s.IndexOf(">", i);
+                i = s.IndexOf("<", j);
+                s = s.Substring(j + 1, i - j - 1);
+                //load file and get exact name and put it in s
+            }
+            catch
+            { }
+            //if match found
+            if (false)
+            {
+                HttpClient cl = new HttpClient();
+                s = Uri.EscapeDataString(s);
+                string url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=28.661904,77.2232688&radius=50000&type=point_of_interest&keyword=" + s + "&key=AIzaSyBMzL7mptHo33PNsuKmT9xKppNgkXotBOM";
+
+                try
+                {
+                    s = await cl.GetStringAsync(url);
+                }
+                catch (Exception ex)
+                {
+
+                }
+                //match not found so it not a monument
+            }
+            else
+            {
+                VisionServiceClient cl = new VisionServiceClient("db82ef68dc95459fad7b46d7a50bb944");
+
+                VisualFeature[] vf = new VisualFeature[] { VisualFeature.Tags};
+                AnalysisResult ar = await cl.AnalyzeImageAsync(blobUrl, vf);
+                Tag[] f = ar.Tags;
+                int i = 0;
+                bool tex = false;
+                foreach (Tag t in f)
+                {
+                    if (t.Name.Contains("text"))
+                    {
+                        tex = true;
+                        break;
+                    }
+                }
+                if (tex)
+                {
+                    try
+
+                    {
+                        string text = "";
+                        cl = new VisionServiceClient("db82ef68dc95459fad7b46d7a50bb944");
+                        OcrResults ocr = await cl.RecognizeTextAsync(blobUrl);
+                        foreach (Region r in ocr.Regions)
+                        {
+                            foreach (Line l in r.Lines)
+                            {
+                                foreach (Word w in l.Words)
+                                {
+                                    text = text + " " + w.Text;
+                                }
+                            }
+                        }
+                    }
+                    catch(Exception ex)
+                    { }
+                }
+                
+            }
+
+
         }
+        private async void PreviewControl_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (!_isPreviewing) return;
+
+            if (!_isFocused && _mediaCapture.VideoDeviceController.FocusControl.FocusState != MediaCaptureFocusState.Searching)
+            {
+                var smallEdge = Math.Min(Window.Current.Bounds.Width, Window.Current.Bounds.Height);
+
+                // Choose to make the focus rectangle 1/4th the length of the shortest edge of the window
+                var size = new Size(smallEdge / 4, smallEdge / 4);
+                var position = e.GetPosition(sender as UIElement);
+
+                // Note that at this point, a rect at "position" with size "size" could extend beyond the preview area. The following method will reposition the rect if that is the case
+                await TapToFocus(position, size);
+            }
+            else
+            {
+                await TapUnfocus();
+            }
+        }
+
+        private async Task TapUnfocus()
+        {
+            _isFocused = false;
+
+            var roiControl = _mediaCapture.VideoDeviceController.RegionsOfInterestControl;
+            await roiControl.ClearRegionsAsync();
+
+            var focusControl = _mediaCapture.VideoDeviceController.FocusControl;
+            await focusControl.FocusAsync();
+        }
+
+        public async Task TapToFocus(Point position, Size size)
+        {
+            _isFocused = true;
+
+            var previewRect = GetPreviewStreamRectInControl();
+            var focusPreview = ConvertUiTapToPreviewRect(position, size, previewRect);
+
+            // Note that this Region Of Interest could be configured to also calculate exposure 
+            // and white balance within the region
+            var regionOfInterest = new RegionOfInterest
+            {
+                AutoFocusEnabled = true,
+                BoundsNormalized = true,
+                Bounds = focusPreview,
+                Type = RegionOfInterestType.Unknown,
+                Weight = 100,
+            };
+
+
+            var focusControl = _mediaCapture.VideoDeviceController.FocusControl;
+            var focusRange = focusControl.SupportedFocusRanges.Contains(AutoFocusRange.FullRange) ? AutoFocusRange.FullRange : focusControl.SupportedFocusRanges.FirstOrDefault();
+            var focusMode = focusControl.SupportedFocusModes.Contains(FocusMode.Single) ? FocusMode.Single : focusControl.SupportedFocusModes.FirstOrDefault();
+            var settings = new FocusSettings { Mode = focusMode, AutoFocusRange = focusRange };
+            focusControl.Configure(settings);
+
+            var roiControl = _mediaCapture.VideoDeviceController.RegionsOfInterestControl;
+            await roiControl.SetRegionsAsync(new[] { regionOfInterest }, true);
+
+            await focusControl.FocusAsync();
+        }
+
+        public Rect GetPreviewStreamRectInControl()
+        {
+            var result = new Rect();
+
+            var previewResolution = _mediaCapture.VideoDeviceController.GetMediaStreamProperties(MediaStreamType.VideoPreview) as VideoEncodingProperties;
+
+            // In case this function is called before everything is initialized correctly, return an empty result
+            if (PreviewControl == null || PreviewControl.ActualHeight < 1 || PreviewControl.ActualWidth < 1 ||
+                previewResolution == null || previewResolution.Height == 0 || previewResolution.Width == 0)
+            {
+                return result;
+            }
+
+            var streamWidth = previewResolution.Width;
+            var streamHeight = previewResolution.Height;
+
+            // For portrait orientations, the width and height need to be swapped
+           
+
+            // Start by assuming the preview display area in the control spans the entire width and height both (this is corrected in the next if for the necessary dimension)
+            result.Width = PreviewControl.ActualWidth;
+            result.Height = PreviewControl.ActualHeight;
+
+            // If UI is "wider" than preview, letterboxing will be on the sides
+            if ((PreviewControl.ActualWidth / PreviewControl.ActualHeight > streamWidth / (double)streamHeight))
+            {
+                var scale = PreviewControl.ActualHeight / streamHeight;
+                var scaledWidth = streamWidth * scale;
+
+                result.X = (PreviewControl.ActualWidth - scaledWidth) / 2.0;
+                result.Width = scaledWidth;
+            }
+            else // Preview stream is "wider" than UI, so letterboxing will be on the top+bottom
+            {
+                var scale = PreviewControl.ActualWidth / streamWidth;
+                var scaledHeight = streamHeight * scale;
+
+                result.Y = (PreviewControl.ActualHeight - scaledHeight) / 2.0;
+                result.Height = scaledHeight;
+            }
+
+            return result;
+        }
+
+        private Rect ConvertUiTapToPreviewRect(Point tap, Size size, Rect previewRect)
+        {
+            // Adjust for the resulting focus rectangle to be centered around the position
+            double left = tap.X - size.Width / 2, top = tap.Y - size.Height / 2;
+
+            // Get the information about the active preview area within the CaptureElement (in case it's letterboxed)
+            double previewWidth = previewRect.Width, previewHeight = previewRect.Height;
+            double previewLeft = previewRect.Left, previewTop = previewRect.Top;
+
+            // Transform the left and top of the tap to account for rotation
+            
+            // For portrait orientations, the information about the active preview area needs to be rotated
+           
+            // Normalize width and height of the focus rectangle
+            var width = size.Width / previewWidth;
+            var height = size.Height / previewHeight;
+
+            // Shift rect left and top to be relative to just the active preview area
+            left -= previewLeft;
+            top -= previewTop;
+
+            // Normalize left and top
+            left /= previewWidth;
+            top /= previewHeight;
+
+            // Ensure rectangle is fully contained within the active preview area horizontally
+            left = Math.Max(left, 0);
+            left = Math.Min(1 - width, left);
+
+            // Ensure rectangle is fully contained within the active preview area vertically
+            top = Math.Max(top, 0);
+            top = Math.Min(1 - height, top);
+
+            // Create and return resulting rectangle
+            return new Rect(left, top, width, height);
+        }
+
+
         protected async override void OnNavigatedFrom(NavigationEventArgs e)
         {
             await CleanupCameraAsync();
@@ -99,6 +350,7 @@ namespace VRDreamer
 
                 _displayRequest.RequestActive();
                 DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
+                
             }
             catch (UnauthorizedAccessException)
             {
@@ -157,5 +409,7 @@ namespace VRDreamer
         {
             MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
         }
+
+        
     }
 }
---
+++ VRDreamer/visionApi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRDreamer
+{
+
+    //public class Tag
+    //{
+    //    public string name { get; set; }
+    //    public double confidence { get; set; }
+    //}
+
+    //public class Caption
+    //{
+    //    public string text { get; set; }
+    //    public double confidence { get; set; }
+    //}
+
+    //public class Description
+    //{
+    //    public List<string> tags { get; set; }
+    //    public List<Caption> captions { get; set; }
+    //}
+
+    //public class Metadata
+    //{
+    //    public int width { get; set; }
+    //    public int height { get; set; }
+    //    public string format { get; set; }
+    //}
+
+    //public class visionApi
+    //{
+    //    public List<Tag> tags { get; set; }
+    //    public Description description { get; set; }
+    //    public string requestId { get; set; }
+    //    public Metadata metadata { get; set; }
+    //}
+
+    
+}
