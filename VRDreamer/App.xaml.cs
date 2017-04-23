6b271de72200cdfad892d6c2f9b523889e35361d
akshaygupta94 <akshaygupta1994@outlook.com>
2017-04-23 15:06:02 +0530

    mistake last me

--- VRDreamer/App.xaml.cs
+++ VRDreamer/App.xaml.cs
@@ -88,7 +88,7 @@ namespace VRDreamer
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(monument_Detail), e.Arguments);
+                    rootFrame.Navigate(typeof(Login_Page), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
